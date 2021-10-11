9698d911f7c5b3496251c02ddec69b5ec4d2c0f6
GrigoreCatalin <grigore.catalin31@yahoo.com>
2021-10-11 20:47:15 +0300

    RingBuffer

---
+++ src/RingBuffer.java
@@ -0,0 +1,69 @@
+public class RingBuffer<E> {
+    private Object[] array;
+    int size;
+    int read;
+    int write;
+
+    public RingBuffer(int cappacity) {
+        array = new Object[cappacity];
+    }
+
+    public void offer(E e) {
+
+        this.array[write] = e;
+        this.write++;
+
+        if (this.write == array.length) {
+            this.write = 0;
+        }
+        if (this.size == array.length) {
+            this.read++;
+            if (this.read == array.length)
+                this.read = 0;
+            return;
+        }
+        this.size++;
+    }
+
+    public E poll() {
+        if (this.size > 0) {
+            E element = (E) array[this.read];
+            this.read++;
+            if (this.read == array.length) {
+                this.read = 0;
+            }
+            this.size--;
+            return element;
+        }
+
+        return null;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public E peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        if (this.read > this.write) {
+            return (E) array[this.read];
+        }
+            return (E) array[this.write];
+    }
+
+    public void display() {
+        if (this.read >= this.write && this.size != 0) {
+            for (int i = this.read; i < this.array.length; i++) {
+                System.out.print(array[i] + " ");
+            }
+        } else if (this.write >= this.read && this.size != 0) {
+            for (int i = this.read; i < this.write; i++) {
+                System.out.print(array[i] + " ");
+            }
+            }
+        }
+
+
+}
---
+++ src/main.java
@@ -0,0 +1,27 @@
+public class main {
+    public static void main(String[] args) {
+
+        RingBuffer queue = new RingBuffer(3);
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        queue.offer(5);
+
+        queue.poll();
+        queue.poll();
+        queue.poll();
+
+
+
+       System.out.println("READ: " + queue.read);
+       System.out.println("WRITE: " + queue.write);
+       System.out.println("SIZE: " + queue.size);
+//        System.out.println(queue.isEmpty());
+//        System.out.println(queue.peek());
+
+        queue.display();
+
+
+    }
+}
